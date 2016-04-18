2c83db1cda4512d40d202ff915594db16df157e0
Tapan <tapan@agilemedialab.in>
2016-04-18 18:11:03 +0530

    first commit

--- fakerTest1.py
+++ fakerTest1.py
@@ -11,14 +11,14 @@ dbConfig = {
 }
 
 fakerConfig = {
-	'postsCount': 1
+	'postsCount': 1,
+	'tagsCount'	: 1
 }
 
 
-def createQuery(text, ES, US, MX, CO, postType, url, special, published):
+def createPostsQuery(text, ES, US, MX, CO, postType, url, special, published):
 
-	QUERY = {}
-	QUERY['wp_posts'] = (
+	QUERY = (
 		"INSERT INTO 	`wp_posts` ("
 		"	`text`,"
 		"	`published`,"
@@ -34,7 +34,14 @@ def createQuery(text, ES, US, MX, CO, postType, url, special, published):
 
 	return QUERY
 
+def createTagsQuery(tagName):
+	QUERY = (
+		"INSERT INTO `wp_tags` ("
+		"	`name` "
+		")	VALUES	('{}')"
+		.format(tagName))
 
+	return QUERY
 
 postTypes = ['normal', 
 			 'ecommerce', 
@@ -56,27 +63,35 @@ cursor = connection.cursor()
 
 for x in range(fakerConfig['postsCount']):
 	queryConfig = {
-	'text'		: fake.text(max_nb_chars=100000),
-	'published' : fake.date_time_between(start_date="-6y", end_date="now"),
-	'ES' 		: randint(0,1),
-	'US' 		: randint(0,1),
-	'MX' 		: randint(0,1),
-	'CO' 		: randint(0,1),
-	'postType' 	: postTypes[randint(0,9)],
-	'url' 		: fake.uri(),
-	'special' 	: randint(0,1)
+		'text'		: fake.text(max_nb_chars=100000),
+		'published' : fake.date_time_between(start_date="-6y", end_date="now"),
+		'ES' 		: randint(0,1),
+		'US' 		: randint(0,1),
+		'MX' 		: randint(0,1),
+		'CO' 		: randint(0,1),
+		'postType' 	: postTypes[randint(0,9)],
+		'url' 		: fake.uri(),
+		'special' 	: randint(0,1)
 	}
 
-	QUERIES = {}
-	QUERIES = createQuery(**queryConfig)
+	DBQuery = {}
+	DBQuery = createPostsQuery(**queryConfig)
 
-	# print(QUERIES['wp_posts'])
-
-	cursor.execute(QUERIES['wp_posts'])
-
-	print("Query executed")
-
+	cursor.execute(DBQuery)
 	connection.commit()
+	print("Posts Query Executed")
+
+for x in range(fakerConfig['tagsCount']):
+	queryConfig = {
+		'tagName'	: fake.pystr(max_chars=20)
+	}
+
+	DBQuery = {}
+	DBQuery = createTagsQuery(**queryConfig)
+
+	cursor.execute(DBQuery)
+	connection.commit()
+	print("Tags Query Executed")
 
 print("Data Populated")
 
